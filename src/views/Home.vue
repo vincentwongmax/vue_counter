a1031e494d6d366e177a0c446aa15dc0a9b026f0
almboe <almboe@almboedeMacBook-Air.local>
2020-09-20 15:50:27 +0800

    vue max v2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="flex flex-col h-screen items-center justify-center">
+      <button type="button" class="btn btn-outline-danger mb-2" >{{count}}</button>
+      <div class="mb-2">
+        <button type="button" class="btn btn-warning" @click="min">-</button>
+        <button type="button" class="btn btn-primary" @click="add">+</button>
+      </div>
+    <button v-show="!isFresh" type="button" class="btn btn-secondary btn-sm " @click="zero">
+      清除</button>
+  </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return { // 變數
+      name: 'Jonathan',
+      count: 0,
+      isFresh: true,
+    };
+  },
+  methods: { // function
+    add() {
+      if (this.count >= 0) {
+        this.count += 1;
+        this.isFresh = false;
+      }
+    // test
+    },
+    min() {
+      if (this.count <= 0) {
+        this.count = 0;
+      } else {
+        this.count -= 1;
+        this.isFresh = false;
+      }
+    },
+    zero() {
+      this.count = 0;
+      this.isFresh = true;
+    },
   },
 };
 </script>
